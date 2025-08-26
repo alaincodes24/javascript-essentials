2a9d61594a8b62c68f6324976cc1d7cbe5cef0bd
alaincodes24 <alaincodes24@gmail.com>
2025-08-26 11:49:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let password = "";
+let correctPassword = "1234";
+
+// while(password !== correctPassword) {
+//   password = prompt("Enter password");
+// }
+
+// console.log("Correct password", correctPassword);
+// alert("Wow, correct password 💃");
+
+
+// Calculator
+
+// const num1 = prompt("Enter the first number");
+// const num2 = prompt("Enter the second number");
+// const sum = Number(num1) + Number(num2);
+// alert(`Calculated Sum: ${sum}`);
+
+// const age = '12';
+// const grade = "B";
+// console.log(age, Number(age));
+// console.log(Number(grade));
+
+
+// Template Literals
+
+const firstName = "Patrick";
+const lastName = "Iradukunda";
+
+console.log("My name is " + firstName + " " + lastName);
+console.log(`My name is ${firstName} ${lastName}`);
+
+const a = 11;
+const b = 5;
+console.log(`The product of ${a} and ${b} is ${a * b}`);
+
+const gender = 'M';
+
+console.log(`I am a ${gender === 'F' ? 'female' : 'male'}`);
+
+
